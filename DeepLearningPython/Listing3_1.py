5fb0d7212c783a78ef6622653513f41674ad845f
Ling Jin <lingjin@gmail.com>
2018-12-27 14:19:28 -0600

    3-1 update 2

--- DeepLearningPython/Listing3_1.py
+++ DeepLearningPython/Listing3_1.py
@@ -3,3 +3,7 @@ from keras.datasets import imdb
 print(train_data[0])
 print(train_labels[0])
 print(max([max(sequence) for sequence in train_data]))
+word_index = imdb.get_word_index()
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
+print(decoded_review)
