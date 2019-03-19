6f349d5e4102859621d845ccfd6d822fb4917223
Ling Jin <lingjin@gmail.com>
2019-03-19 06:52:59 -0500

    LIst 3.3

--- DeepLearningPython/Listing3_3.py
+++ DeepLearningPython/Listing3_3.py
@@ -1,6 +1,10 @@
 from keras import models
 from keras import layers
+
 model = models.Sequential()
 model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
 model.add(layers.Dense(16, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='rmsprop',
+    loss='binary_crossentropy',
+    metrics=['accuracy'])
