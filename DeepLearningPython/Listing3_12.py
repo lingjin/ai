06995b3dcf469fce6bdc6b723512fb827bbffa29
Ling Jin <lingjin@gmail.com>
2019-03-22 07:08:28 -0500

    Listing 3.12

---
+++ DeepLearningPython/Listing3_12.py
@@ -0,0 +1,6 @@
+from keras.datasets import reuters
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+print(len(train_data))
+print(len(test_data))
+print(train_data[10])
