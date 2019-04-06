de195f1d812dc84343600fcd293a6fbe00e008ef
Ling Jin <lingjin@gmail.com>
2019-04-06 14:27:33 -0500

    List 3.23

---
+++ DeepLearningPython/Listing3_23.py
@@ -0,0 +1,42 @@
+from keras.datasets import reuters
+(train_data, train_labels), (test_data, test_labels) = reuters.load_data(num_words=10000)
+
+import numpy as np
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
+
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+def to_one_hot(labels, dimension=46):
+    results = np.zeros((len(labels), dimension))
+    for i, label in enumerate(labels):
+        results[i, label] = 1.
+    return results
+one_hot_train_labels = to_one_hot(train_labels)
+one_hot_test_labels = to_one_hot(test_labels)
+
+from keras import models
+from keras import layers
+
+x_val = x_train[:1000]
+partial_x_train = x_train[1000:]
+y_val = one_hot_train_labels[:1000]
+partial_y_train = one_hot_train_labels[1000:]
+
+model = models.Sequential()
+model.add(layers.Dense(64, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(4, activation='relu'))
+model.add(layers.Dense(46, activation='softmax'))
+model.compile(optimizer='rmsprop',
+    loss='categorical_crossentropy',
+    metrics=['accuracy'])
+model.fit(partial_x_train,
+    partial_y_train,
+    epochs=20,
+    batch_size=128,
+    validation_data=(x_val, y_val))
