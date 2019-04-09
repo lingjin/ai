6d4315b4732f3ff0c1c5928d799c837f9596f830
Ling Jin <lingjin@gmail.com>
2019-04-09 07:13:53 -0500

    List 3.25

---
+++ DeepLearningPython/Listing3_25.py
@@ -0,0 +1,13 @@
+from keras.datasets import boston_housing
+(train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
+
+print(train_data.shape)
+print(test_data.shape)
+print(train_targets)
+
+mean = train_data.mean(axis=0)
+train_data -= mean
+std = train_data.std(axis=0)
+train_data /= std
+test_data -= mean
+test_data /= std
